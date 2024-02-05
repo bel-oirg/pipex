dc9ec7eaf21823a0f237970a7fcbc19fcfe01957
Badr Eddine El Oirghi <bel-oirg@e1r3p1.1337.ma>
2024-02-05 18:55:51 +0100

    added vinaya

--- main.c
+++ main.c
@@ -6,25 +6,28 @@
 /*   By: bel-oirg <bel-oirg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 01:49:39 by bel-oirg          #+#    #+#             */
-/*   Updated: 2024/02/04 21:30:35 by bel-oirg         ###   ########.fr       */
+/*   Updated: 2024/02/05 18:50:39 by bel-oirg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	buddha(t_cmd *cmd)
+static int	buddha(t_cmd *cmd)
 {
-	int	piped;
 	int	fd[2];
 	int	forked;
 
-	piped = pipe(fd);
+	if (pipe(fd) < 0)
+		return (perror(ERR_PIPE), 1);
 	forked = fork();
+	if (forked < 0)
+		return (close(fd[0]), close(fd[1]), perror(ERR_FORK), 1);
 	if (!forked)
 	{
 		close(fd[0]);
 		dup2(fd[1], STDOUT_FILENO);
 		execve(cmd->flags[0], cmd->flags, NULL);
+		return (close(fd[0]), close(fd[1]), perror(ERR_EXEC), 1);
 	}
 	else
 	{
@@ -32,25 +35,43 @@ static void	buddha(t_cmd *cmd)
 		dup2(fd[0], STDIN_FILENO);
 		waitpid(forked, NULL, 0);
 	}
+	return (0);
 }
 
-int	main(int argc, char *argv[], char *envp[])
+void	err(int *fd_in, int *fd_out)
 {
-	t_cmd	*cmd;
+	close(*fd_in);
+	close(*fd_out);
+	my_malloc(0, 0);
+}
+
+void	vinaya(int argc, char *argv[], t_cmd *cmd)
+{
 	int		index;
 	int		fd_in;
 	int		fd_out;
 
 	index = 2;
+	fd_in = open(argv[1], O_RDONLY, 0777);
+	fd_out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (dup2(fd_in, STDIN_FILENO) < 0)
+		(1) && (perror(ERR_DUP), err(&fd_in, &fd_out), 0);
+	while (++index < argc - 1)
+	{
+		(buddha(cmd)) && (err(&fd_in, &fd_out), 0);
+		cmd = cmd->next;
+	}
+	if (dup2(fd_out, STDOUT_FILENO) < 0)
+		(1) && (perror(ERR_DUP), err(&fd_in, &fd_out), 0);
+	execve(cmd->flags[0], cmd->flags, NULL);
+}
+
+int	main(int argc, char *argv[], char *envp[])
+{
+	t_cmd	*cmd;
+
 	cmd = NULL;
 	check_args(argc, argv);
 	get_cmds(argc, argv, envp, &cmd);
-	fd_in = open(argv[1], O_RDONLY, 0777);
-	fd_out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
-	dup2(fd_in, STDIN_FILENO);
-	while(++index  < argc - 1)
-		buddha(cmd),
-		cmd = cmd->next;
-	dup2(fd_out, STDOUT_FILENO);
-	execve(cmd->flags[0], cmd->flags, NULL);
+	vinaya(argc, argv, cmd);
 }
---
+++ parsed_utils.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsed_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bel-oirg <bel-oirg@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 17:07:11 by bel-oirg          #+#    #+#             */
+/*   Updated: 2024/02/05 17:10:06 by bel-oirg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+int	arr_len(char **splited)
+{
+	int	index;
+
+	if (!splited)
+		return (0);
+	index = 0;
+	while (splited[index])
+		index++;
+	return (index);
+}
+
+char	*my_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	if (!to_find[0])
+		return (str);
+	while (str[++i])
+	{
+		j = 0;
+		while (str[i + j] && str[i + j] == to_find[j])
+		{
+			if (!to_find[j + 1])
+				return (&str[i + j + 1]);
+			++j;
+		}
+	}
+	return (0);
+}
+
+int	is_it_in(char *str, char c)
+{
+	while (str && *str)
+		if (*str++ == c)
+			return (1);
+	return (0);
+}
